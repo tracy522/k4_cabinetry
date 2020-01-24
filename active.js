bd2e22343ac8f6ba6b61006b21300bc3787f50ed
tracy522 <42328352+tracy522@users.noreply.github.com>
2020-01-23 17:53:07 -0700

    Add files via upload

---
+++ active.js
@@ -0,0 +1,14 @@
+// JavaScript Document to add active class to navigation
+jQuery(document).ready(function($){
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
+  
+  // Account for home page with empty path
+  if ( path == '' ) {
+    path = 'index.html';
+  }
+      
+  var target = $('nav a[href="'+path+'"]');
+  // Add active class to target link
+  target.addClass('active');
+});
